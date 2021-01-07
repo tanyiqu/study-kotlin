a2811f84b294cca5a1feb2e91d0aee1c82ea6e09
Tanyiqu <1953649096@qq.com>
2021-01-07 13:06:37 +0800

    变量、常量、类型

---
+++ kotlin-study/src/02-TypeIntro.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var experience: Int = 5
+    experience += 5;
+    println(experience)
+
+    val name: String = "Tanyiqu"
+    println(name)
+
+    var Experience = 6;
+    println(Experience)
+
+}
